296a9bfb463127a54463450019fbf03e08b5e3aa
Michal Stapor <kmd.gld@edlund.dk>
2023-03-27 18:00:36 +0200

    Restructuring code - clean up

---
+++ Space_Invaders/images.py
@@ -0,0 +1,17 @@
+import pygame
+import os
+
+# Load images
+RED_SPACE_SHIP = pygame.image.load(os.path.join('Space_Invaders/assets', 'pixel_ship_red_small.png'))
+GREEN_SPACE_SHIP = pygame.image.load(os.path.join('Space_Invaders/assets', 'pixel_ship_green_small.png'))
+BLUE_SPACE_SHIP = pygame.image.load(os.path.join('Space_Invaders/assets', 'pixel_ship_blue_small.png'))
+
+# PLayer ship
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join('Space_Invaders/assets', 'pixel_ship_yellow.png'))
+
+# Lasers
+RED_LASER = pygame.image.load(os.path.join('Space_Invaders/assets', 'pixel_laser_red.png'))
+GREEN_LASER = pygame.image.load(os.path.join('Space_Invaders/assets', 'pixel_laser_green.png'))
+BLUE_LASER = pygame.image.load(os.path.join('Space_Invaders/assets', 'pixel_laser_blue.png'))
+YELLOW_LASER = pygame.image.load(os.path.join('Space_Invaders/assets', 'pixel_laser_yellow.png'))
+
